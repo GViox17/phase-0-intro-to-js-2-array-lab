4b3d9172881120276a03eefaf8247c3a4d2331c8
Gabriel Viox <gviox17@gmail.com>
2021-08-23 00:30:26 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]// Write your solution here!
+
+function destructivelyAppendCat(){
+    cats.push('Ralph');
+}
+function destructivelyPrependCat(){
+    cats.unshift('Bob');
+}
+function destructivelyRemoveLastCat(){
+    cats.pop('Garfield');
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift('Milo');
+}
+function appendCat(){
+    return[...cats, 'Broom'];
+}
+
+function prependCat(){
+    return['Arnold',...cats];
+
+}
+function removeLastCat() {
+    const RemoveLastCatArray = cats.slice(0, cats.length - 1);
+    return RemoveLastCatArray;
+    
+}
+function removeFirstCat(){
+    const NewFirstCatArray = cats.slice(1);
+    return NewFirstCatArray;
+}
